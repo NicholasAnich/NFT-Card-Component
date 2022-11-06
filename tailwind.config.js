ef7df91c8c0aa64d60e31890f8ab23ffcc1a9fc9
lufarius <infinitevolume@gmail.com>
2022-11-06 00:02:50 -0700

    First initial commit and Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        fontFamily: {
+            sans: ["Outfit", "sans-serif"],
+        },
+        extend: {},
+        color: {
+            softBlue: "#8BACD9",
+            white: "#FFFFFF",
+            cyan: "#00FFF8",
+            "veryDarkBlue-main-BG": "#0D192C",
+            "veryDarkBlue-card-BG": "#15263F",
+            "veryDarkBlue-line": "#2E405A",
+        },
+    },
+    plugins: [],
+};
